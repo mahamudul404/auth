bb788d5dfe5180f0f939363cb69b7bbebda9b225
mahamudul404 <mahamudul408432@gmail.com>
2025-11-02 00:07:30 +0600

    view page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgentController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
